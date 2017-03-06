8436e9d367e7ede6562a218a23932d7a653e13df
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-03-06 22:54:08 +0530

    Feature: Basic CLI with clipboard copy, output to file or console added

--- cli.js
+++ cli.js
@@ -2,6 +2,9 @@
 
 const yargs = require('yargs');
 const Img2tab = require('img2tab');
+const clipboardy = require('clipboardy');
+const fs = require('fs');
+const path = require('path');
 
 const argv = yargs
   .usage('$0 <command>')
@@ -12,6 +15,29 @@ const argv = yargs
     .describe('o', 'Path to output file')
   .alias('c', 'clipboard')
     .describe('c', 'Copy to clipboard')
+    .boolean('c')
+  .alias('w', 'width')
+    .describe('w', 'Width per pixel')
+  .alias('hi', 'height')
+    .describe('hi', 'Height per pixel')
   .help('h')
   .alias('h', 'help')
   .argv;
+
+
+const source = argv.s;
+const width = argv.wi;
+const height = argv.hi;
+
+const img2tabInst = new Img2tab(source, width, height);
+
+img2tabInst.getTable().then((table) => {
+  if (argv.c) {
+    clipboardy.writeSync(table);
+  } else if (argv.o) {
+    const outputPath = path.join(__dirname, argv.o);
+    fs.writeFileSync(outputPath, table);
+  } else {
+    console.log(table);
+  }
+}).catch(err => console.log(`${err}`));
